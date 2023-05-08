438dd2b0de9f429a5c8d3f1d9799e0cabe517642
simolorevg <simoneviaggi@libero.it>
2023-05-08 13:10:26 +0200

    request to postman done

--- server.php
+++ server.php
@@ -1,4 +1,9 @@
 <?php
-$json_value = file_get_contents('dischi.json');
-$disk_array = json_decode($json_value, true);
-var_dump($disk_array);
+$disk_value = file_get_contents('dischi.json');
+$disk_array = json_decode($disk_value, true);
+$disk_response = [
+    'results' => $disk_array
+];
+var_dump($response);
+header('Content-Type: application/json');
+echo json_encode($disk_response);
