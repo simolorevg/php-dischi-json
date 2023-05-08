d7412eda8ae2186059333c5a19f5deab1091a2f0
simolorevg <simoneviaggi@libero.it>
2023-05-08 14:46:39 +0200

    Working on index page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--BOOTSTRAP CSS LINK-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!--VUE JS LINK-->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!--AXIOS LINK-->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <title>Dischi</title>
+</head>
+
+<body>
+    <div id='app'>
+        <h1>From Disco to Disco</h1>
+    </div>
+</body>
+
+</html>
